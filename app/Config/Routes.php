e9f29ebeb1351ecd2c5e2892a09c5407447d7619
Daud Dhiya <daud.dhiya31@gmail.com>
2023-12-17 16:02:52 +0700

    feat: add user views

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,12 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('/about', 'Home::about');
+$routes->get('/contact', 'Home::contact');
+$routes->get('/home', 'Home::home');
+$routes->get('/login', 'Home::login');
+$routes->get('/register', 'Home::register');
+$routes->get('/profile', 'Home::profile');
+$routes->get('/logout', 'Home::logout');
+$routes->get('/paymentform', 'Home::paymentform');
+$routes->get('/paymenttotal', 'Home::paymenttotal');
